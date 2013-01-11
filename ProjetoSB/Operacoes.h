bdbfebd466cbcafa2704e55e7ba257a1fc642695
willian <willian@willian-asus-laptop>
2013-01-11 19:44:24 -0200

    paradas hardcore de map da lista de operações

--- ProjetoSB/Operacoes.h
+++ ProjetoSB/Operacoes.h
@@ -17,15 +17,31 @@
 
 #include <string>
 #include <list>
+#include <map>
 
 using namespace std;
+
+static const map<string,ItemOperacao> montaOperacoes () {
+	map<string,ItemOperacao> mapOp;
+	mapOp["add"] = {"add", R, "000000", "010000", "x"};
+	mapOp["sub"] = {"sub", R, "000000", "010010", "x"};;
+	mapOp["adds"] = {"add.s", FR, "010001", "000000", "10000"};
+	mapOp["addd"] = {"add.d", FR, "010001", "000001", "10001"};
+	mapOp["subs"] = {"sub.s", FR, "010001", "000000", "10000"};
+	mapOp["subd"] = {"sub.d", FR, "010001", "000001", "10001"};
+	mapOp["label"] = {"x", LABEL, "x", "x", "x"};
+	return mapOp;
+}
+
+static const map<string,ItemOperacao> listaOperacao = montaOperacoes();
+
 
 static const struct ItemOperacao add = {"add", R, "000000", "010000", "x"};
 static const struct ItemOperacao sub = {"sub", R, "000000", "010010", "x"};
 static const struct ItemOperacao adds = {"add.s", FR, "010001", "000000", "10000"};
 static const struct ItemOperacao addd = {"add.d", FR, "010001", "000001", "10001"};
-static const struct ItemOperacao subs = {"add.s", FR, "010001", "000000", "10000"};
-static const struct ItemOperacao subd = {"add.s", FR, "010001", "000001", "10001"};
+static const struct ItemOperacao subs = {"sub.s", FR, "010001", "000000", "10000"};
+static const struct ItemOperacao subd = {"sub.d", FR, "010001", "000001", "10001"};
 static const struct ItemOperacao label = {"x", LABEL, "x", "x", "x"};
 
 class Operacao {
