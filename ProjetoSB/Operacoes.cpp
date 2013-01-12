b21c5b6f2d148f63d4c9b26cfa11f41943b10b35
willian <willian@willian-asus-laptop>
2013-01-11 22:54:57 -0200

    fix dos registradores

--- ProjetoSB/Operacoes.cpp
+++ ProjetoSB/Operacoes.cpp
@@ -32,9 +32,9 @@ OperacaoBinaria::OperacaoBinaria (list<string> tokens) {
 
 void OperacaoBinaria::montaRegistradoresR(string& saida) {
 	ItemRegistrador irs, irt, ird;
-	if ((irs = encontraRegistrador(rs)).codigo.compare(none.codigo)
-			|| (irt = encontraRegistrador(rt)).codigo.compare(none.codigo)
-			|| (ird = encontraRegistrador(rd)).codigo.compare(none.codigo))
+	if (!(irs = encontraRegistrador(rs)).codigo.compare(none.codigo)
+			|| !(irt = encontraRegistrador(rt)).codigo.compare(none.codigo)
+			|| !(ird = encontraRegistrador(rd)).codigo.compare(none.codigo))
 		throw runtime_error("registradores inexistentes");
 
 	saida += irs.codigo;
